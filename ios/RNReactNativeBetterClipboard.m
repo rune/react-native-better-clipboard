04d05f23848bc84def01457373f12926fcb2f89d
Matt Bretl <mattbretl@users.noreply.github.com>
2021-02-15 20:45:36 -0800

    fix: check clipboard.strings (not clipboard.hasStrings) (#3)

--- ios/RNReactNativeBetterClipboard.m
+++ ios/RNReactNativeBetterClipboard.m
@@ -13,18 +13,10 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(getStrings: (RCTPromiseResolveBlock)resolve rejecter:(__unused RCTPromiseRejectBlock)reject)
 {
   UIPasteboard *clipboard = [UIPasteboard generalPasteboard];
-  if (@available(iOS 10.0, *)) {
-    resolve(@{
-      @"string": clipboard.hasStrings ? clipboard.string : @"",
-      @"strings": clipboard.hasStrings ? clipboard.strings : @[]
-    });
-  } else {
-    // Fallback on earlier versions
-    resolve(@{
-      @"string": clipboard.string ? clipboard.string : @"",
-      @"strings": clipboard.strings ? clipboard.strings : @[]
-    });
-  }
+  resolve(@{
+    @"string": clipboard.string ? clipboard.string : @"",
+    @"strings": clipboard.strings ? clipboard.strings : @[]
+  });
 }
 
 @end
